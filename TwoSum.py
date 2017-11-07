b3801a0c3fee0a2dbb830914a13d2d211ee01c36
jiaojiening <953786895@qq.com>
2017-11-07 19:32:35 +0800

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -5,10 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-		for i in range(0, len(nums)-1 ):
-		    num1 = nums[i]
-			for j in range(i+1, len(nums)):
-				num2 = nums[j]
-				y = num1 + num2
-				if y == target:
-					return i, j
+        ls=[]
+        for i in range(0, len(nums)):
+            item = target - nums[i]
+            nums[i] = "done"
+            if item in nums:
+                ls.append(i)
+                ls.append(nums.index(item))
+        return ls
